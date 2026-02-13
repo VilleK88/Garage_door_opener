d33d768336bc109ba5a53064139aca8fef13bbfa
ville <ville.karppinen88@gmail.com>
2026-02-13 12:43:31 +0200

    Project updated

--- src/StepMotor.cpp
+++ src/StepMotor.cpp
@@ -1,6 +1,18 @@
 #include "StepMotor.h"
+#include "hardware/gpio.h"
 
-void StepMotor::step(int direction) {
+StepMotor::StepMotor(const std::array<uint, 4>& pins)
+    : coil_pins(pins) {}
+
+void StepMotor::init_coil_pins() const {
+    for (auto& coil : coil_pins) {
+        gpio_init(coil);
+        gpio_set_dir(coil, GPIO_OUT);
+        gpio_put(coil, 0);
+    }
+}
+
+void StepMotor::step(int direction) const {
     const int half_step[8][4] = {
         {1, 0, 0, 0},
         {1, 1, 0, 0},
@@ -14,5 +26,7 @@ void StepMotor::step(int direction) {
 
     static int phase = 0;
     phase = phase + direction & 7;
-    for (int i = 0; i < )
+    for (int i = 0; i < COIL_PINS_SIZE; i++) {
+        gpio_put(coil_pins[i], half_step[phase][i]);
+    }
 }
--- src/StepMotor.h
+++ src/StepMotor.h
@@ -1,10 +1,24 @@
 #ifndef STEPMOTOR_H
 #define STEPMOTOR_H
+#include <iostream>
+#include <array>
+#include "pico/types.h"
 
+#define IN1 2
+#define IN2 3
+#define IN3 6
+#define IN4 13
+#define COIL_PINS_SIZE 4
+// Array grouping all coil control pins for iteration
+static constexpr int coil_pins[] = {IN1, IN2, IN3, IN4};
 
 class StepMotor {
 public:
-    void step(int direction);
+    explicit StepMotor(const std::array<uint, 4>& pins);
+    void init_coil_pins() const;
+    void step(int direction) const;
+private:
+    std::array<uint, 4> coil_pins;
 };
 
 
